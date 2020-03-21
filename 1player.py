de9cc64ea15fba112a794cf5c572a211f226b231
armaanlalani <60145425+armaanlalani@users.noreply.github.com>
2020-03-20 21:29:28 -0400

    Update 1player.py

--- 1player.py
+++ 1player.py
@@ -5,7 +5,7 @@ printBoard(board)
 
 while True: 
    pieces = GetPlayerPositions(board,10)
-   print(pieces) #displays the pieces available to move
+   print(pieces) # displays the pieces available to move
    legal = False
    while legal == False:
       player = int(input("WHITE: Select the piece you wish to move: "))
@@ -16,14 +16,14 @@ while True:
    moves.sort()
    print(moves)
    legal = False
-   while legal == False: # error checking for moves that are not allowed
+   while legal == False: # allows the user to choose the move they wish to make
       move = int(input("WHITE: Select the position you wish to move to: (Enter -1 to go back)"))
       if move == -1:
          break
       for i in moves:
          if move == i:
             legal = True
-   while move == -1:
+   while move == -1: # if the user wishes to go back
       pieces = GetPlayerPositions(board,10)
       print(pieces)
       legal = False
@@ -36,7 +36,7 @@ while True:
       moves.sort()
       print(moves)
       legal = False
-      while legal == False:
+      while legal == False: # allows the user to choose the position they wish to move to
          move = int(input("WHITE: Select the position you wish to move to: (Enter -1 to go back)"))
          if move == -1:
             break
@@ -51,16 +51,16 @@ while True:
    b = list(board)
    Move(b,player,move)
    while check(b,10):
-      move = int(input("You are in check, please try again"))
+      move = int(input("You are in check, please try again")) # if the move the player makes puts them in or keeps them in the 'check' position, they are required to make another move
       b[player] = board[player]
       b[move] = board[move]
    Move(board,player,move)
    printBoard(board)
-   if checkmate(board,20):
+   if checkmate(board,20): # checks game ending condition
       print("WHITE WINS THE GAME")
       break
 
-   opponent = chessPlayer(board,20)
+   opponent = chessPlayer(board,20) # instantiates the computer's move
    print(opponent[0:3])
    print("Opponent Move")
    print("From: " + str(opponent[1][0]))
@@ -73,8 +73,8 @@ while True:
    print("checkmate: " + str(CheckMateValue(board,opponent[1][0],opponent[1][1],20)))
    print("protect: " + str(Protect(board,opponent[1][0],opponent[1][1],20)))
    print("inthreat: " + str(InThreat(board,opponent[1][0],opponent[1][1],20)))
-   Move(board,int(opponent[1][0]),int(opponent[1][1]))
+   Move(board,int(opponent[1][0]),int(opponent[1][1])) # performs the best possible move available
    printBoard(board)
-   if checkmate(board,10):
+   if checkmate(board,10): # checks game ending condition
       print("BLACK WINS THE GAME")
       break
